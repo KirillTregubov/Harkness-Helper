66b9f8206e7418d535471a4169c8ae84b00e5b57
KTregubov <31662934+KTregubov@users.noreply.github.com>
2019-05-31 20:55:48 -0400

    Fixed dependencies and cleaned up backend code
    Co-Authored-By: charlottevolk <charlottevolk@users.noreply.github.com>

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,16 +2,16 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const morgan = require('morgan');
-const express_graphql = require('express-graphql');
-var Harkness = require('../models/post');
+const mongoose = require('mongoose');
+var Harkness = require('../models/harkness.js');
 
 const app = express();
 app.use(morgan('combined'));
 app.use(bodyParser.json());
 app.use(cors());
 
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/harkness');
+
+mongoose.connect('mongodb://localhost:27017/harkness', { useNewUrlParser: true });
 var db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error"));
 db.once("open", function(callback){
