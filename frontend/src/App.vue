2d9d813b37a6ff46d7665f7ea85479339165754c
KTregubov <31662934+KTregubov@users.noreply.github.com>
2019-06-05 12:51:40 -0400

    Update App.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,5 @@
 <template>
-  <!-- <body id="app"> -->
-  <!-- <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-  </div>-->
   <router-view/>
-  <!-- </body> -->
 </template>
 
 <style lang="scss">
