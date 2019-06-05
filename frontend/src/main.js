851d4e0f09ebb3d2daf98ecbde352ce0c241ceb9
Alex Alexiev <aalexiev@bayviewglen.ca>
2019-06-05 13:16:10 -0400

    initialized firebase error

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,7 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import { rtdbPlugin } from 'vuefire'
-import firebase from 'firebase' // change
+import firebase from 'firebase' 
+import {db} from '@/firebase'
 
 Vue.config.productionTip = false
 
